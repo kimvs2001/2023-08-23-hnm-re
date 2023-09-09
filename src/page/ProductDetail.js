c89c876af0df8d9978cb471946337e0a0b0ea791
Kimjuhyeong <kimvs2001@naver.com>
2023-09-09 20:07:28 +0900

    add reduct-middleware productDetail

--- src/page/ProductDetail.js
+++ src/page/ProductDetail.js
@@ -4,18 +4,27 @@ import { useParams } from 'react-router-dom'
 import Dropdown from 'react-bootstrap/Dropdown';
 import DropdownButton from 'react-bootstrap/DropdownButton';
 import Button from 'react-bootstrap/Button';
+import { useDispatch, useSelector } from 'react-redux';
+import { productAction } from '../redux/actions/productAction';
 
 const ProductDetail = () => {
 
   let {id} = useParams();
-  const [product,setProduct] = useState(null);
+  //const [product,setProduct] = useState(null);
   
+  const dispatch = useDispatch();
+  
+  const product = useSelector(state=>state.product);
+  
+
   const getProductDetail= async()=>{
-    let url = `http://localhost:5000/products/${id}`
-    let response = await fetch(url);
-    let data = await response.json();
-    console.log('data',data);
-    setProduct(data);
+    // let url = `http://localhost:5000/products/${id}`
+    // let response = await fetch(url);
+    // let data = await response.json();
+    // console.log('data',data);
+    // setProduct(data);
+    
+    dispatch(productAction.getProductDetail(id))
   }
   useEffect(()=>{
     getProductDetail();
--- src/redux/actions/productAction.js
+++ src/redux/actions/productAction.js
@@ -8,5 +8,15 @@ function getProducts(searchQuery){
     dispatch({type:"GET_PRODUCT_SUCCESS",payload:{data}});
    }; 
 }
+function getProductDetail(id){
+   return async (dispatch,getState)=>{
+      let url = `https://my-json-server.typicode.com/kimvs2001/2023-08-23-hnm-re/products/${id}`
+      let response = await fetch(url);
+      let data = await response.json();
+      //console.log('data',data);
 
-export const productAction={getProducts};
+      dispatch({type:"GET_PRODUCT_DETAIL_SUCCESS",payload:{data}});
+   }
+}
+
+export const productAction={getProducts,getProductDetail};
